9d74ec8c5af6b92b3630cf0ba5308926a8004665
Alberto Dragoni <albertodragoni@yahoo.it>
2020-02-19 17:10:04 +0100

    prima parte, funziona

--- js/main.js
+++ js/main.js
@@ -1,1 +1,16 @@
+// Prima parte - ammissione
+//preparare lista invitati
+//chiedere nome (mail) all'utente
+//verificare se il nome è nell'elenco, se non lo è, gioco finito.
+//se lo è, stampare la scritta 'ok, sei il benvenuto'
+var listaInvitati = ['Luca', 'Giovanni', 'Patrizia', 'Stefano', 'Elena', 'Federica', 'Giorgio']
+var ammissioneFesta = prompt ('Inserisca il suo nome, prego');
 
+for (var i = 0; i < listaInvitati.length; i++) {
+    console.log(listaInvitati[i])
+    if (ammissioneFesta == listaInvitati[i]) {
+        alert ('Benvenuto, la stavamo aspettando con ansia')
+    } else {
+        alert ('Mi spiace, lei non è in lista')
+    }
+}
